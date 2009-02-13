2f31d4f6ac1815d1199094d961db511904dbe463
irivera <irivera>
2009-02-13 19:37:22 +0000

    prototype v0.1 added some ontologies update some classes
    git-svn-id: https://svn.forge.morfeo-project.org/fast-fp7project@41 d8b0c59b-6544-0410-94d9-83a5658a058e

---
+++ trunk/catalogue/src/eu/morfeoproject/fast/restserver/CatalogueGUI.java
@@ -0,0 +1,183 @@
+package eu.morfeoproject.fast.restserver;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+
+public class CatalogueGUI extends JFrame {
+	
+	private static final long serialVersionUID = 6163545064005769050L;
+	
+	private static final int HEIGHT = 120;
+	private static final int WIDTH = 600;
+	
+    private JButton startButton;
+    private JButton stopButton;
+    private JLabel dirLabel;
+    private JTextField dirTextField;
+    private JButton dirButton;
+    private JLabel portLabel;
+    private JTextField portTextField;
+    
+    private CatalogueServer server;
+    
+    public CatalogueGUI() {
+		super("FAST Catalogue Server");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {	}
+	    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	    int screenHeight = screenSize.height;
+	    int screenWidth = screenSize.width;
+	    setLocation((screenWidth-WIDTH)/2, (screenHeight-HEIGHT)/2);
+	    setSize(WIDTH, HEIGHT);
+//	    setResizable(false);
+	    
+	    dirLabel = new JLabel("Repository directory: ");
+	    dirTextField = new JTextField();
+	    // select directory button
+	    dirButton = new JButton("Select...");
+	    dirButton.addActionListener(
+	    		new ActionListener() {
+	    			public void actionPerformed(ActionEvent e) {
+	    				showDirectoryChooser();
+	    			}
+	    		}
+	    );
+	    
+	    portLabel = new JLabel("Server port: ");
+	    portTextField = new JTextField("8082");
+	    
+	    // start button
+	    startButton = new JButton("Start server");
+	    startButton.addActionListener(
+	    		new ActionListener() {
+	    			public void actionPerformed(ActionEvent e) {
+	    				startServer();
+	    			}
+	    		}
+	    );
+	    
+	    // stop button
+	    stopButton = new JButton("Stop server");
+	    stopButton.setEnabled(false);
+	    stopButton.addActionListener(
+	    		new ActionListener() {
+	    			public void actionPerformed(ActionEvent e) {
+	    				stopServer();
+	    			}
+	    		}
+	    );
+
+	    // add elements to the screen
+	    getContentPane().setLayout(new BorderLayout());
+	    JPanel mainPanel = new JPanel();
+	    mainPanel.setLayout(new FlowLayout());
+	    JPanel panel1 = new JPanel();
+	    panel1.setLayout(new FlowLayout());
+	    panel1.add(dirLabel);
+	    dirTextField.setColumns(30);
+	    panel1.add(dirTextField);
+	    panel1.add(dirButton);
+	    JPanel panel2 = new JPanel();
+	    panel2.setLayout(new FlowLayout());
+	    panel2.add(portLabel);
+	    portTextField.setColumns(8);
+	    panel2.add(portTextField);
+	    JPanel panel3 = new JPanel();
+	    panel3.setLayout(new FlowLayout());
+	    panel3.add(startButton);
+	    panel3.add(stopButton);
+
+	    mainPanel.add(panel1);
+	    mainPanel.add(panel2);
+	    mainPanel.add(panel3);
+	    getContentPane().add(mainPanel);
+		
+		setVisible(true);
+	}
+	
+	private void startServer() {
+		setModeStarted(true);
+		// do the real work
+		File dir = new File(dirTextField.getText());
+		int port = Integer.parseInt(portTextField.getText());
+		server = new CatalogueServer(port, dir);
+		try {
+			server.startServer();
+		} catch (Exception e) {
+			setModeStarted(false);
+			showErrorDialog("Server couldn't be started. Check the parameters.", "Server Error");
+		}
+	}
+	
+	private void stopServer() {
+		setModeStarted(false);
+		// do the real work
+		try {
+			server.stopServer();
+		} catch (Exception e) {
+			setModeStarted(true);
+			showErrorDialog("Server couldn't be stopped. Try again.", "Server Error");
+		}
+	}
+	
+	private void showDirectoryChooser() {
+		JFileChooser fc = new JFileChooser();
+		fc.setCurrentDirectory(new java.io.File("."));
+		fc.setDialogTitle("Select a directory for the repository...");
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		fc.setAcceptAllFileFilterUsed(false);
+		if (fc.showOpenDialog(getContentPane()) == JFileChooser.APPROVE_OPTION) {
+			dirTextField.setText(fc.getSelectedFile().getAbsolutePath());
+		}
+	}
+	
+	private void setModeStarted(boolean started) {
+		if (started) {
+			startButton.setEnabled(false);
+			stopButton.setEnabled(true);
+			dirTextField.setEnabled(false);
+			dirButton.setEnabled(false);
+			portTextField.setEnabled(false);
+		} else {
+			startButton.setEnabled(true);
+			stopButton.setEnabled(false);
+			dirTextField.setEnabled(true);
+			dirButton.setEnabled(true);
+			portTextField.setEnabled(true);
+		}
+	}
+	
+	private void showInfoDialog(String msg, String title) {
+		JOptionPane.showMessageDialog(getContentPane(), msg, title, JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	private void showWarningDialog(String msg, String title) {
+		JOptionPane.showMessageDialog(getContentPane(), msg, title, JOptionPane.WARNING_MESSAGE);
+	}
+	
+	private void showErrorDialog(String msg, String title) {
+		JOptionPane.showMessageDialog(getContentPane(), msg, title, JOptionPane.ERROR_MESSAGE);
+	}
+	
+	/* main */
+	public static void main(String[] args) {
+		new CatalogueGUI();
+	}
+	
+}
---
+++ trunk/catalogue/src/eu/morfeoproject/fast/restserver/CheckRestlet.java
@@ -0,0 +1,151 @@
+package eu.morfeoproject.fast.restserver;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.ontoware.rdf2go.model.node.URI;
+import org.ontoware.rdf2go.model.node.impl.URIImpl;
+import org.openrdf.repository.RepositoryException;
+import org.restlet.data.MediaType;
+import org.restlet.data.Method;
+import org.restlet.data.Request;
+import org.restlet.data.Response;
+import org.restlet.data.Status;
+
+import eu.morfeoproject.fast.catalogue.Catalogue;
+import eu.morfeoproject.fast.catalogue.NotFoundException;
+import eu.morfeoproject.fast.catalogue.OntologyInvalidException;
+import eu.morfeoproject.fast.model.Condition;
+import eu.morfeoproject.fast.model.Screen;
+
+public class CheckRestlet extends CatalogueRestlet {
+
+	static Logger logger = Logger.getLogger(CheckRestlet.class);
+	
+	public CheckRestlet(Catalogue catalogue) throws RepositoryException {
+		super();
+		this.catalogue = catalogue;
+	}
+	
+
+	@Override
+	public void handle(Request request, Response response) {
+		if (request.getMethod().equals(Method.POST)) {
+			logger.debug("Entering CHECK operation...");
+			try {
+				// creates JSON representation of the input
+				JSONObject input = new JSONObject(request.getEntity().getText());
+				// parses the canvas
+				Set<Screen> canvas = new HashSet<Screen>();
+				JSONArray jsonCanvas = input.getJSONArray("canvas");
+				for (int i = 0; i < jsonCanvas.length(); i++) {
+					URI uri = new URIImpl(((JSONObject)jsonCanvas.get(i)).getString("uri"));
+					Screen s = catalogue.getScreen(uri);
+					if (s == null)
+						throw new NotFoundException("Screen "+uri+" does not exist.");
+					canvas.add(s); 
+				}
+				// parses the list of elements
+				Set<Screen> elements = new HashSet<Screen>();
+				JSONArray jsonElements = input.getJSONArray("elements");
+				for (int i = 0; i < jsonElements.length(); i++) {
+					URI uri = new URIImpl(((JSONObject)jsonElements.get(i)).getString("uri"));
+					Screen s = catalogue.getScreen(uri);
+					if (s == null)
+						throw new NotFoundException("Screen "+uri+" does not exist.");
+					elements.add(s); 
+				}
+				// parses the domain context
+				JSONObject jsonDomainContext = input.getJSONObject("domainContext");
+				JSONArray jsonTags = jsonDomainContext.getJSONArray("tags");
+				Set<URI> tags = new HashSet<URI>();
+				for (int i = 0; i < jsonTags.length(); i++)
+					tags.add(catalogue.getOrCreateTag(jsonTags.getString(i)));
+				StringBuffer sb = new StringBuffer();
+				for (URI tag : tags)
+					sb.append(tag+" ");
+				// TODO do something with the user
+				String user = jsonDomainContext.getString("user");
+				// parses the criterion
+				String criterion = input.getString("criterion");
+				
+				// do the real work
+				Set<Screen> reachables = catalogue.filterReachableScreens(canvas);
+				JSONObject output = new JSONObject();
+				if (criterion.equalsIgnoreCase("reachability")) {
+					JSONArray canvasOut = new JSONArray();
+					boolean reachability = true;
+					for (Screen s : canvas) {
+						reachability = true;
+						JSONObject jsonScreen = new JSONObject();
+						jsonScreen.put("uri", s.getUri());
+						JSONArray preArray = new JSONArray();
+						for (Condition c : s.getPreconditions()) {
+							JSONObject jsonPre = new JSONObject();
+							jsonPre.put("expression", c.toString());
+							boolean satisfied = false;
+							if (reachables.contains(s))
+								satisfied = true;
+							else
+								satisfied = catalogue.isSatisfied(reachables, c, true, true, null);
+							jsonPre.put("satisfied", satisfied);
+							reachability = reachability && satisfied;
+							preArray.put(jsonPre);
+						}
+						jsonScreen.put("reachability", reachability);
+						jsonScreen.put("preconditions", preArray);
+						canvasOut.put(jsonScreen);
+					}
+					output.put("canvas", canvasOut);
+					JSONArray elementsOut = new JSONArray();
+					for (Screen s : elements) {
+						reachability = true;
+						JSONObject jsonScreen = new JSONObject();
+						jsonScreen.put("uri", s.getUri());
+						JSONArray preArray = new JSONArray();
+						for (Condition c : s.getPreconditions()) {
+							JSONObject jsonPre = new JSONObject();
+							jsonPre.put("expression", c.toString());
+							boolean satisfied = catalogue.isSatisfied(reachables, c, true, true, null);
+							jsonPre.put("satisfied", satisfied);
+							reachability = reachability && satisfied;
+							preArray.put(jsonPre);
+						}
+						jsonScreen.put("reachability", reachability);
+						jsonScreen.put("preconditions", preArray);
+						elementsOut.put(jsonScreen);
+					}
+					output.put("elements", elementsOut);
+					response.setEntity(output.toString(2), MediaType.APPLICATION_JSON);
+					response.setStatus(Status.SUCCESS_OK);
+				} else {
+					response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST, "Critetion not allowed.");
+				}
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST);
+			} catch (NotFoundException e) {
+				response.setStatus(Status.CLIENT_ERROR_BAD_REQUEST, e.getMessage());
+			} catch (RepositoryException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (OntologyInvalidException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			logger.debug("...Exiting CHECK operation");
+		} else {
+			response.setStatus(Status.CLIENT_ERROR_METHOD_NOT_ALLOWED);
+		}
+}
+}
