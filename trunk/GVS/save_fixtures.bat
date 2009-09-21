9afa32b61147938ba1776963cb2487c1fb092cd5
Fabio <jfgs77@gmail.com>
2009-09-21 14:08:04 +0000

    GVS: Fixtures (Amazon)
    git-svn-id: https://svn.forge.morfeo-project.org/fast-fp7project@284 d8b0c59b-6544-0410-94d9-83a5658a058e

---
+++ trunk/GVS/save_fixtures.bat
@@ -0,0 +1,2 @@
+python manage.py dumpdata --format=xml --indent=4 buildingblock > buildingblock\fixtures\initial_data.xml
+python manage.py dumpdata --format=xml --indent=4 auth user > fast\fixtures\initial_data.xml
