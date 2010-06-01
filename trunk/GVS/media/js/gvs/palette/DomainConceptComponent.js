8591b5a76ee949856ed69ab58f82a579989fb88e
Fabio <jfgs77@gmail.com>
2010-06-01 08:46:58 +0000

    GVS: Domain Concept Tooltip
    git-svn-id: https://svn.forge.morfeo-project.org/fast-fp7project@829 d8b0c59b-6544-0410-94d9-83a5658a058e

--- trunk/GVS/media/js/gvs/palette/DomainConceptComponent.js
+++ trunk/GVS/media/js/gvs/palette/DomainConceptComponent.js
@@ -23,7 +23,53 @@ var DomainConceptComponent = Class.create(PaletteComponent,
      * @override
      */
     _createView: function () {
-        return new DomainConceptView(this._buildingBlockDescription);
+        var view = new DomainConceptView(this._buildingBlockDescription);
+        this._createTooltip(view.getNode(), this._buildingBlockDescription);
+        return view;
+    },
+
+    /**
+     * Creates a new Tooltip for the component view
+     * @type BuildingBlockView
+     * @override
+     */
+    _createTooltip: function (/*DomNode View*/ node) {
+        var conceptName = this._buildingBlockDescription.getTitle();
+
+        var content = document.createElement('div');
+        var title = document.createElement('h3');
+        title.appendChild(document.createTextNode(conceptName+' '));
+        content.appendChild(title);
+
+        if (this._buildingBlockDescription['description']) {
+            var languages = $H(this._buildingBlockDescription['description']).keys();
+            if (languages.size() > 0) {
+                var description = this._buildingBlockDescription['description'][languages[0]];
+            } else {
+                var description = this._buildingBlockDescription['description'];
+            }
+            var descriptionElement = document.createElement('p');
+            descriptionElement.appendChild(document.createTextNode(description));
+            content.appendChild(descriptionElement);
+        }
+
+        if (this._buildingBlockDescription['subClassOf']) {
+            var subClassOf = document.createElement('h4');
+            subClassOf.appendChild(document.createTextNode('Subclass of:'));
+            content.appendChild(subClassOf);
+            content.appendChild(document.createTextNode(this._buildingBlockDescription['subClassOf']));
+        }
+
+        if (this._buildingBlockDescription['uri']) {
+            var subClassOf = document.createElement('h4');
+            subClassOf.appendChild(document.createTextNode('URI:'));
+            content.appendChild(subClassOf);
+            content.appendChild(document.createTextNode(this._buildingBlockDescription['uri']));
+        }
+
+        var tip = new dijit.Tooltip({connectId:[node],
+            label:'<div style="max-width:300px">'+content.innerHTML+'</div>'});
+        tip.startup();
     },
 
     /**
