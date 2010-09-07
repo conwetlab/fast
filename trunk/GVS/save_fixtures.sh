c0cfd420a8266ee96c0c3bff10b00ab2aab364d7
Javier López <jlopez@fi.upm.es>
2010-09-07 14:32:54 +0000

    bug in save_fixtures.sh
    git-svn-id: https://svn.forge.morfeo-project.org/fast-fp7project@955 d8b0c59b-6544-0410-94d9-83a5658a058e

--- trunk/GVS/save_fixtures.sh
+++ trunk/GVS/save_fixtures.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ./manage.py dumpdata --settings=settings --format=xml --indent=4 buildingblock > buildingblock\fixtures\initial_data.xml
-python ./manage.py dumpdata --settings=settings --format=xml --indent=4 auth user > fast\fixtures\initial_data.xml
+python ./manage.py dumpdata --settings=settings --format=xml --indent=4 buildingblock > buildingblock/fixtures/initial_data.xml
+python ./manage.py dumpdata --settings=settings --format=xml --indent=4 auth user > fast/fixtures/initial_data.xml
