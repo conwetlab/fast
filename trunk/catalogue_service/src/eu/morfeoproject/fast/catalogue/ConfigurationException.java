c7e2909ff6212a64b29b9d075ab6162c55e52757
irivera <irivera>
2010-11-25 14:13:35 +0000

    renamed
    git-svn-id: https://svn.forge.morfeo-project.org/fast-fp7project@1070 d8b0c59b-6544-0410-94d9-83a5658a058e

---
+++ trunk/catalogue_service/src/eu/morfeoproject/fast/catalogue/ConfigurationException.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package eu.morfeoproject.fast.catalogue;
+
+/**
+ * @author Ismael Rivera
+ *
+ */
+public class ConfigurationException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 
+	 */
+	public ConfigurationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public ConfigurationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param cause
+	 */
+	public ConfigurationException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public ConfigurationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
