2ed2f88b008e8740da08c53b05ea0488185a12e3
irivera <irivera>
2011-01-18 22:17:23 +0000

    supports a single object {"uri": "..."} or an array of them [{"uri": "..."}, {...}, ...]
    git-svn-id: https://svn.forge.morfeo-project.org/fast-fp7project@1147 d8b0c59b-6544-0410-94d9-83a5658a058e

--- trunk/catalogue_service/src/eu/morfeoproject/fast/catalogue/services/CopyServlet.java
+++ trunk/catalogue_service/src/eu/morfeoproject/fast/catalogue/services/CopyServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.ontoware.rdf2go.model.node.URI;
@@ -50,13 +51,26 @@ public class CopyServlet extends GenericServlet {
 		// Create a new entry in the collection where the ID is assigned automatically by 
 		// the collection and it is returned.
 		try {
-			JSONObject json = new JSONObject(body);
-			URI bbUri = new URIImpl(json.getString("uri"));
-			URI copyUri = getCatalogue().createCopy(bbUri);
-			response.setContentType(MediaType.APPLICATION_JSON);
-			json.put("uri", bbUri);
-			json.put("copy", copyUri);
-			writer.print(json.toString(2));
+			String output = null;
+			try {
+				JSONArray json = new JSONArray(body);
+				for (int idx = 0; idx < json.length(); idx++) {
+					JSONObject element = json.getJSONObject(idx);
+					URI bbUri = new URIImpl(element.getString("uri"));
+					URI copyUri = getCatalogue().createCopy(bbUri);
+					response.setContentType(MediaType.APPLICATION_JSON);
+					element.put("copy", copyUri);
+				}
+				output = json.toString(2);
+			} catch (JSONException e) {
+				JSONObject json = new JSONObject(body);
+				URI bbUri = new URIImpl(json.getString("uri"));
+				URI copyUri = getCatalogue().createCopy(bbUri);
+				response.setContentType(MediaType.APPLICATION_JSON);
+				json.put("copy", copyUri);
+				output = json.toString(2);
+			}
+			writer.print(output);
 			response.setStatus(HttpServletResponse.SC_OK);
 		} catch (NotFoundException e) {
 			log.error(e.toString(), e);
