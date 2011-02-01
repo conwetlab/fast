b57c40ee7cb19c9324b4e097ab968640d2f624b8
irivera <irivera>
2011-02-01 20:54:33 +0000

    removes classes and properties which don't belong to the FAST ontology (created erroneously by the VocabularyWriter)
    git-svn-id: https://svn.forge.morfeo-project.org/fast-fp7project@1241 d8b0c59b-6544-0410-94d9-83a5658a058e

--- trunk/catalogue_service/src/eu/morfeoproject/fast/catalogue/vocabulary/FGO.java
+++ trunk/catalogue_service/src/eu/morfeoproject/fast/catalogue/vocabulary/FGO.java
@@ -24,14 +24,6 @@ public interface FGO {
     public static final URI Operator = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#Operator", false);
 
     /**
-     */
-    public static final URI Person = new URIImpl("http://xmlns.com/foaf/0.1/Person", false);
-
-    /**
-     */
-    public static final URI Tag = new URIImpl("http://commontag.org/ns#Tag", false);
-
-    /**
      * Label: Action@en 
      * Comment: An action represents a specific routine which will be performed when a certain
 condition is fulfilled within a certain screen component. Examples are methods of a Web service (e.g., getItem) or functionality to update or change the contents of a form.@en 
@@ -81,18 +73,10 @@ condition is fulfilled within a certain screen component. Examples are methods o
     public static final URI Pipe = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#Pipe", false);
 
     /**
-     */
-    public static final URI OnlineAccount = new URIImpl("http://xmlns.com/foaf/0.1/OnlineAccount", false);
-
-    /**
      * Label: With Post-condition@en 
      * Comment: Those kinds of building blocks which can have post-conditions.@en 
      */
     public static final URI WithPostConditions = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#WithPostConditions", false);
-
-    /**
-     */
-    public static final URI User = new URIImpl("http://rdfs.org/sioc/ns#User", false);
 
     /**
      */
@@ -117,16 +101,6 @@ condition is fulfilled within a certain screen component. Examples are methods o
     public static final URI WithCode = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#WithCode", false);
 
     /**
-     * Label: With Definition@en 
-     * Comment: Any kind of building block that can be defined declaratively in the GVS.@en 
-     */
-    public static final URI WithDefinition = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#WithDefinition", false);
-
-    /**
-     */
-    public static final URI Organization = new URIImpl("http://xmlns.com/foaf/0.1/Organization", false);
-
-    /**
      * Label: Backend Service@en 
      * Comment: A Web service which provides data and/or functionality to a screen. The actual backend service is external to FAST, and only available through a wrapper (the service Resource).@en 
      */
@@ -144,18 +118,6 @@ A condition is made up of individual facts, where each fact is represented inter
      * Comment: A form is the visual aspect of a screen: its user interface. Each form is made up of individual form elements.@en 
      */
     public static final URI Form = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#Form", false);
-
-    /**
-     */
-    public static final URI Image = new URIImpl("http://xmlns.com/foaf/0.1/Image", false);
-
-    /**
-     */
-    public static final URI accountName = new URIImpl("http://xmlns.com/foaf/0.1/accountName", false);
-
-    /**
-     */
-    public static final URI name = new URIImpl("http://xmlns.com/foaf/0.1/name", false);
 
     /**
      * Label: has pattern string@en 
@@ -190,30 +152,6 @@ A condition is made up of individual facts, where each fact is represented inter
     public static final URI hasLanguage = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#hasLanguage", false);
 
     /**
-     */
-    public static final URI title = new URIImpl("http://purl.org/dc/terms/title", false);
-
-    /**
-     */
-    public static final URI mbox_sha1sum = new URIImpl("http://xmlns.com/foaf/0.1/mbox_sha1sum", false);
-
-    /**
-     */
-    public static final URI revision = new URIImpl("http://usefulinc.com/ns/doap#revision", false);
-
-    /**
-     */
-    public static final URI modified = new URIImpl("http://purl.org/dc/terms/modified", false);
-
-    /**
-     */
-    public static final URI description = new URIImpl("http://purl.org/dc/terms/description", false);
-
-    /**
-     */
-    public static final URI created = new URIImpl("http://purl.org/dc/terms/created", false);
-
-    /**
      * Label: contains@en 
      * Comment: Many kinds of components in FAST can contain other components: screenflows contain screens, screens contain forms, operators and resources, forms contain form elements, etc.@en 
      * Comment: http://purl.oclc.org/fast/ontology/gadget#BuildingBlock 
@@ -238,20 +176,12 @@ A condition is made up of individual facts, where each fact is represented inter
     public static final URI hasOperator = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#hasOperator", false);
 
     /**
-     */
-    public static final URI creator = new URIImpl("http://purl.org/dc/terms/creator", false);
-
-    /**
      * Label: has form@en 
      * Comment: If a screen is defined declaratively, this property links it to its form (i.e., its visual user interface).@en 
      * Comment: http://purl.oclc.org/fast/ontology/gadget#Screen 
      * Range: http://purl.oclc.org/fast/ontology/gadget#Form 
      */
     public static final URI hasForm = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#hasForm", false);
-
-    /**
-     */
-    public static final URI tagged = new URIImpl("http://commontag.org/ns#tagged", false);
 
     /**
      * Label: from@en 
@@ -283,14 +213,6 @@ A condition is made up of individual facts, where each fact is represented inter
      * Range: http://purl.oclc.org/fast/ontology/gadget#BuildingBlock 
      */
     public static final URI hasPrototype = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#hasTemplate", false);
-
-    /**
-     */
-    public static final URI depiction = new URIImpl("http://xmlns.com/foaf/0.1/depiction", false);
-
-    /**
-     */
-    public static final URI interest = new URIImpl("http://xmlns.com/foaf/0.1/interest", false);
 
     /**
      * Label: to@en 
@@ -325,10 +247,6 @@ A condition is made up of individual facts, where each fact is represented inter
     public static final URI hasPreCondition = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#hasPreCondition", false);
 
     /**
-     */
-    public static final URI rights = new URIImpl("http://purl.org/dc/terms/rights", false);
-
-    /**
      * Label: has pattern@en 
      * Comment: This property links the abstract representation of a fact (i.e., an RDF triple) in FAST to a graph resource containing the actual triple.@en 
      * Comment: http://purl.oclc.org/fast/ontology/gadget#Condition 
@@ -337,30 +255,10 @@ A condition is made up of individual facts, where each fact is represented inter
     public static final URI hasPattern = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#hasPattern", false);
 
     /**
-     */
-    public static final URI means = new URIImpl("http://commontag.org/ns#means", false);
-
-    /**
-     */
-    public static final URI mbox = new URIImpl("http://xmlns.com/foaf/0.1/mbox", false);
-
-    /**
-     */
-    public static final URI holdsAccount = new URIImpl("http://xmlns.com/foaf/0.1/holdsAccount", false);
-
-    /**
-     */
-    public static final URI rightsHolder = new URIImpl("http://purl.org/dc/terms/rightsHolder", false);
-
-    /**
      * Label: integratesTerm@en 
      * Comment: A way to explicitly say that an ontology uses terms from another namespace.@en 
      */
     public static final URI integratesTerm = new URIImpl("http://data.semanticweb.org/ns/misc#integratesTerm", false);
-
-    /**
-     */
-    public static final URI subject = new URIImpl("http://purl.org/dc/terms/subject", false);
 
     /**
      * Label: has copy@en 
@@ -385,10 +283,6 @@ A condition is made up of individual facts, where each fact is represented inter
      * Range: http://www.w3.org/2000/01/rdf-schema#Resource 
      */
     public static final URI uses = new URIImpl("http://purl.oclc.org/fast/ontology/gadget#uses", false);
-
-    /**
-     */
-    public static final URI member = new URIImpl("http://xmlns.com/foaf/0.1/member", false);
 
     /**
      * Label: has resource@en 
